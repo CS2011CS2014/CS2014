964af9c46aa97619234d92c46e26c757d933e7ba
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-10-10 09:38:05 +0300

    Initial implementation of send-receive

--- cs281-0837-2010/mpi_practise/mpi_send_recv.c
+++ cs281-0837-2010/mpi_practise/mpi_send_recv.c
@@ -2,6 +2,7 @@
 #include <mpi.h>
 
 int main(){
+    MPI_Init(NULL, NULL);
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
@@ -11,7 +12,7 @@ int main(){
     int number;
     if(world_rank == 0){
         number = -1;
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD;)
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     }else if(world_rank == 1){
         MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Process 1 received umber %d from process 0\n", number);
