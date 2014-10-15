fc1419ba2bb938d0c64f80dcfc02874f2f2b8394
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2014-10-15 08:35:10 +0300

    Pulling updates

---
+++ CS281-0375-2010/MPI Reduce/reduce.c
@@ -0,0 +1,30 @@
+/**
+ * Reg No: CS281-0375/2010
+ * Name: Gedion Kyalo Kitili
+ * 
+ * MPI_Reduce example using MPI
+ */
+
+#include <stdio.h>
+#include "mpi.h"
+
+int main (int argc, char *argv[])
+{
+  int rank, size, recvbuf;
+  MPI_Init (&argc, &argv);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+  
+  printf ("Processor %d submitting my rank for summation \n",rank);
+  
+  MPI_Reduce (&rank,&recvbuf,1,MPI_INTEGER,MPI_SUM,0,MPI_COMM_WORLD);
+  
+  if (rank == 0)
+  {
+    printf ("Processor %d  Total of ranks is: %d \n",rank, recvbuf);
+  }
+  
+  MPI_Finalize ();
+
+}
--- CS281-2150-2010/hello.c
+++ CS281-2150-2010/hello.c
@@ -1,3 +1,4 @@
+/* C Example */
 #include <mpi.h>
 #include <stdio.h>
  
